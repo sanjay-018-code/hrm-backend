5195f74a66a9bbf150592678b04ba249978c40c1
sanjay-018-code <sandi115735p@gmail.com>
2026-08-03 21:44:44 +0530

    Update CORS middleware to include production URL
    Removed local origins and added production URL.

--- app/main.py
+++ app/main.py
@@ -31,10 +31,7 @@ async def database_error_handler(request: Request, error: PyMongoError):
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
+        "https://hrm-frontend-ruddy.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
